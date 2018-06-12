9a3b3d842b944fb3fb7d6488429c4b2f650614d2
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-06-12 21:28:24 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,5 @@
+import React, { Component } from "react";
+
+const App = () => {};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+import "normalize.css";
+import "typeface-roboto";
+
+ReactDOM.render(<App />, document.getElementById("root"));
