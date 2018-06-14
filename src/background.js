d590f062fde700b67993526ae1c5ce07062fe169
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-06-14 16:11:26 +0700

    Create notification after copied

--- src/background.js
+++ src/background.js
@@ -5,10 +5,25 @@ import { store } from "./store/store";
 import { addQuote } from "./store/actions";
 
 chrome.contextMenus.onClicked.addListener(info => {
+  const { selectionText } = info;
+  const maxLength = 27;
+  const textPart = selectionText.substr(0, maxLength);
+  const excerpt =
+    selectionText.length > maxLength ? `${textPart}...` : textPart;
+
   const quote = {
     id: uuid(),
     text: info.selectionText
   };
 
+  const notification = {
+    type: "basic",
+    title: "Successfully copied !",
+    message: excerpt,
+    iconUrl: "./images/icon-48.png"
+  };
+
   store.dispatch(addQuote(quote));
+
+  chrome.notifications.create(uuid(), notification);
 });
