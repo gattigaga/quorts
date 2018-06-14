ab8bb1cd6e2fc3391dad914815a270406d256e84
Gattigaga Hayyuta Dewa <gattigaga@gmail.com>
2018-06-14 13:48:44 +0700

    Create a context menu

--- public/scripts/background.js
+++ public/scripts/background.js
@@ -1,5 +1,17 @@
-chrome.browserAction.onClicked.addListener(() => {
-  chrome.tabs.create({
-    url: chrome.extension.getURL("index.html")
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.contextMenus.create({
+    id: "1",
+    title: "Save to Quorts",
+    contexts: ["selection"]
+  });
+
+  chrome.browserAction.onClicked.addListener(() => {
+    chrome.tabs.create({
+      url: chrome.extension.getURL("index.html")
+    });
+  });
+
+  chrome.contextMenus.onClicked.addListener(info => {
+    alert(info.selectionText);
   });
 });
